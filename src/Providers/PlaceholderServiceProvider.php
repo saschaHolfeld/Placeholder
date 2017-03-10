1281a96b20359554efc305853dfa26e46fefcbae
saschaHolfeld <sascha.holfeld@3wfuture.de>
2017-03-10 15:59:45 +0100

    Placeholder Plugin for

---
+++ src/Providers/PlaceholderServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+// Placeholder/src/Providers/PlaceholderServiceProvider.php
+
+namespace Placeholder\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+
+class PlaceholderServiceProvider extends ServiceProvider
+{
+
+	/**
+	 * Register the service provider.
+	 */
+	public function register()
+	{
+
+	}
+}
+
+?>
